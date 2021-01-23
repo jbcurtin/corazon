7c05b363bc701cfb627e8926d6d640b3d692f7aa
Joe Curtin <42@jbcurtin.io>
2021-01-23 02:30:26 -0500

    fixed up deps for tests. Removed python3.9 requirement: tensorflow doesn't seem to support 3.9

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,6 +1,11 @@
-from corazon.pipeline_search_vet import search_and_vet_one
 
 
 def test_pipeline_1():
-    # TODO: Implement me
-    search_and_vet_one(123, 1, {}, [], plot=False)
+    import tempfile
+
+    from corazon import run_pipeline
+
+    tic_id = 383724012
+    sector = 14
+    out_dir = tempfile.NamedTemporaryFile().name
+    run_pipeline.run_write_one(tic_id, sector, out_dir)
